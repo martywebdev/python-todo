90f55db6f8ba2dab8815520bd69a30279929ee58
martyumlas <martyumlas@gmail.com>
2025-06-17 21:24:54 +0800

    while loop

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
-todos = [] 
+todos = []
 
 while True:
-    user_text = input('Enter todo: ')
+    print(f"Select option\n[1] view todos\n[2] add todo")
 
-    if user_text == 'quit':
-        break
-    else:
-        todos.append(user_text)
+    try:
+        user_input = input('Enter option: ')
+        if user_input == 'quit':
+            break
 
-    print(todos)
+        user_option = int(user_input)
+
+        if user_option == 1:
+            print("Your Todos:")
+            for todo in todos:
+                print(f"- {todo}")
+        elif user_option == 2:
+            todo = input("Enter todo: ")
+            todos.append(todo)
+        else:
+            print("Invalid option. Choose 1 or 2.")
+
+    except ValueError:
+        print("Please enter a valid number (1 or 2) or type 'quit' to exit.")
