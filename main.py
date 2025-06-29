f8496d70ba9f48eb225c32e4c15a8ba7eb8f22af
martyumlas <martyumlas@gmail.com>
2025-06-29 22:29:11 +0800

    update edit and complete cases

--- main.py
+++ main.py
@@ -7,19 +7,16 @@ while True:
         case 'add':
             todo = input('Enter a todo: ') + '\n'
 
-            file = open('todos.txt', 'r')
-            todos = file.readlines() #this is the todos
-            file.close()
+            with open('todos.txt', 'r') as file:
+                todos = file.readlines()
 
             todos.append(todo)
 
-            file = open('todos.txt', 'w')
-            file.writelines(todos)
-            file.close()
+            with open('todos.txt', 'w') as file:
+                file.writelines(todos)
         case 'show':
-            file = open('todos.txt', 'r')
-            todos = file.readlines()
-            file.close()
+            with open('todos.txt', 'r') as file:
+                todos = file.readlines()
 
             # new_todos = [item.strip('\n') for item in todos]
 
@@ -28,11 +25,32 @@ while True:
                 print(f'{index + 1}. {item.title()}')
         case 'edit':
             number = int(input("Number of the todo to edit: ")) - 1
+
+            with open('todos.txt', 'r') as file:
+                todos = file.readlines()
+
             new_todo = input("Enter new todo: ")
-            todos[number] = new_todo
+            todos[number] = new_todo + '\n'
+
+            # print(todos)
+
+            with open('todos.txt', 'w') as file:
+                file.writelines(todos)
         case 'complete':
             number = int(input("Number of the todo to complete: "))
-            todos.pop(int(number) - 1)
+            with open('todos.txt', 'r') as file:
+                todos = file.readlines()
+
+            index = number - 1
+            todo_to_remove = todos[index].strip('\n')
+            todos.pop(index)
+
+            with open('todos.txt', 'w') as file:
+                file.writelines(todos)
+
+            message = f"Todo {todo_to_remove} was removed from the list."
+
+            print(message)
         case 'exit':
             break
         case _:
