18b9669bf3fda782d8831a4f6781f601bd21108b
martyumlas <martyumlas@gmail.com>
2025-07-04 23:06:15 +0800

    bonus 11 for function

---
+++ bonus/bonus11.py
@@ -0,0 +1,23 @@
+def get_average(file_path):
+    with open(file_path, 'r') as file:
+        data = file.readlines()
+    values = data[1:]
+    values = [float(num) for num in values]
+
+    avg = sum(values) / len(values)
+    return avg
+
+average = get_average("files/data.txt")
+print(average)
+
+
+def get_maximum():
+    celsius = [14, 15.1, 12.3]
+    maximum = max(celsius)
+    return maximumaa
+
+
+celsius = get_maximum()
+fahrenheit = celsius * 1.8 + 32
+
+print(fahrenheit)
