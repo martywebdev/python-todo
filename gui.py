c66cb7a55fe305f6a363ff1baf40ede96a650cf1
Marty <martyumlas@gmail.com>
2025-07-15 20:19:24 +0800

    adding images on gui

--- gui.py
+++ gui.py
@@ -2,10 +2,13 @@ import functions
 import FreeSimpleGUI as sg
 import time
 
-clock = sg.Text('')
+sg.theme("Black")
+
+clock = sg.Text('', key="clock")
 label  = sg.Text('Enter Todo')
 text_input =  sg.Input(key='todo')
-add_button = sg.Button('Add', bind_return_key=True)
+add_button = sg.Button(image_source="add.png", bind_return_key=True, size=2, mouseover_colors="LightBlue2",
+                       tooltip="Add Todo", key="Add")
 list_box = sg.Listbox(values=functions.get_todos(), key='todos',
                       enable_events=True, size=(45, 10))
 edit_button = sg.Button('Edit')
@@ -13,6 +16,7 @@ complete_button = sg.Button('Complete')
 exit_button = sg.Button('Exit')
 
 layout =  [
+    [clock],
     [label],
     [text_input, add_button],
     [list_box, edit_button, complete_button],
@@ -22,10 +26,8 @@ layout =  [
 window = sg.Window('My Todo App', layout, font=("Helvetica", 20))
 
 while True:
-    event, values = window.read()
-    # print(event)
-    # print(values)
-
+    event, values = window.read(timeout=200)
+    window['clock'].update(time.strftime("%b %d, %Y %H:%M:%S"))
     match event:
         case 'Add':
             todos = functions.get_todos()
