94d8b279a38525b26119909cb80bf4f33842e6bd
Marty <martyumlas@gmail.com>
2025-07-17 22:11:19 +0800

    add docstrings

--- web.py
+++ web.py
@@ -15,9 +15,9 @@ def add_todo():
     except Exception as e:
         print(e)
 
-# def edit_todo(index):
-#     st.session_state["selected_todo_index"] = index
-#     st.session_state["todo_input"] = todos[index]
+'''def edit_todo(index):
+    st.session_state["selected_todo_index"] = index
+    st.session_state["todo_input"] = todos[index]'''
 
 st.title("My Todo App")
 st.subheader("This is my todo app")
@@ -34,22 +34,22 @@ for index, todo in enumerate(todos):
         del st.session_state[todo]
         st.rerun()
 
-# if remove_indices:
-#     for i in sorted(remove_indices, reverse=True):
-#         todos.pop(i)
-#     functions.write_todos(todos)
+'''if remove_indices:
+    for i in sorted(remove_indices, reverse=True):
+        todos.pop(i)
+    functions.write_todos(todos)'''
 
 
-# this is capturing todo
-# for index, todo in enumerate(todos):
-#     if st.checkbox(todo, key=f"{index}-todo"):
-#         if st.session_state.get("selected_todo_index") != index:
-#             edit_todo(index)
-#
+'''this is capturing todo
+for index, todo in enumerate(todos):
+    if st.checkbox(todo, key=f"{index}-todo"):
+        if st.session_state.get("selected_todo_index") != index:
+            edit_todo(index)
+'''
 st.text_input(label="add", placeholder="Add a new todo", on_change=add_todo,
                      key="new_todo")
 
-# if "todo_input" in st.session_state:
-#     st.text_input("Edit Todo", value=st.session_state["todo_input"], key="todo_input_field")
-
+'''if "todo_input" in st.session_state:
+    st.text_input("Edit Todo", value=st.session_state["todo_input"], key="todo_input_field")
+'''
 st.session_state
