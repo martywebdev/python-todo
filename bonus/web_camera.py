f91f8b66c0df3578fbc9da86dcb299db210412af
Marty <martyumlas@gmail.com>
2025-07-22 19:51:21 +0800

    web cam

---
+++ bonus/web_camera.py
@@ -0,0 +1,13 @@
+import streamlit as st
+from PIL import Image
+print(dir(st.expander))
+with st.expander("Start Camera"):
+    camera_image = st.camera_input('Camera')
+
+
+
+if camera_image:
+    img = Image.open(camera_image)
+    gray_img = img.convert("L")
+    st.image(gray_img)
+
